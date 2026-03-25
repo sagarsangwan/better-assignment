6e1a0582f13c0352e63e6d1c404db98158611650
sagarsangwan <iamsagarsangwan@gmail.com>
2026-03-25 12:15:51 +0530

    backend is running

--- backend/run.py
+++ backend/run.py
@@ -2,11 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
-
-
+    return "<p>Hello, World!</p>"
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
